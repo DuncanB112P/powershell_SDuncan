96d6cbcf6a5fde63d9ac928e3c6aea480ce350c0
Sam Duncan <samuel.p.duncan@gmail.com>
2024-09-12 22:57:00 -0400

    powershell exercises

---
+++ Get-Date_practice.ps1
@@ -0,0 +1,5 @@
+﻿(Get-Content -Path C:\Temp\files_practice.txt)|
+    ForEach-Object {$_ -Replace 'Stubborn', 'Obstinate'}|
+        Set-Content -Path C:\Temp\files_practice.txt
+Get-Content -Path C:\Temp\files_practice.txt
+Write-Output (Get-Content -Path C:\Temp\files_practice.txt)
